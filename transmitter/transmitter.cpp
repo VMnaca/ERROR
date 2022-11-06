1b2103d9e5103e8adf2e8a582e6cdbd1f304f5c5
Vincent Morgan <vincent.morgan.42@gmail.com>
2022-11-06 10:46:17 -0500

    final push

--- transmitter/transmitter.cpp
+++ transmitter/transmitter.cpp
@@ -1,36 +1,3 @@
-/*
-    FM Transmitter - use Raspberry Pi as FM transmitter
-
-    Copyright (c) 2022, Marcin Kondej
-    All rights reserved.
-
-    See https://github.com/markondej/fm_transmitter
-
-    Redistribution and use in source and binary forms, with or without modification, are
-    permitted provided that the following conditions are met:
-
-    1. Redistributions of source code must retain the above copyright notice, this list
-    of conditions and the following disclaimer.
-
-    2. Redistributions in binary form must reproduce the above copyright notice, this
-    list of conditions and the following disclaimer in the documentation and/or other
-    materials provided with the distribution.
-
-    3. Neither the name of the copyright holder nor the names of its contributors may be
-    used to endorse or promote products derived from this software without specific
-    prior written permission.
-
-    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
-    EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
-    OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
-    SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-    INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
-    TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
-    BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-    CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
-    WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
-
 #include "transmitter.hpp"
 #include "mailbox.h"
 #include <bcm_host.h>
@@ -111,7 +78,7 @@ class Peripherals
         }
     private:
         Peripherals() {
-            int memFd;
+            int memrFd;
             if ((memFd = open("/dev/mem", O_RDWR | O_SYNC)) < 0) {
                 throw std::runtime_error("Cannot open /dev/mem file (permission denied)");
             }
@@ -348,8 +315,6 @@ void Transmitter::TransmitViaDma(Reader &reader, ClockOutput &output, unsigned s
         dmaCb[cbOffset].nextCbAddress = allocated.GetPhysicalAddress(&dmaCb[cbOffset + 1]);
         cbOffset++;
 
-        std::cout << value << std::endl;
-
         dmaCb[cbOffset].transferInfo = DMA_TI_NO_WIDE_BURST | DMA_TI_PERMAP(0x5) | DMA_TI_DEST_DREQ | DMA_TI_WAIT_RESP;
         dmaCb[cbOffset].srcAddress = allocated.GetPhysicalAddress(pwmFifoData);
         dmaCb[cbOffset].dstAddress = peripherals.GetPhysicalAddress(&pwm.GetFifoIn());
@@ -384,7 +349,6 @@ void Transmitter::TransmitViaDma(Reader &reader, ClockOutput &output, unsigned s
             eof = data.size() < bufferSize;
             for (std::size_t i = 0; i < data.size(); i++) {
                 float value = data[i].GetMonoValue();
-                std::cout << value << std::endl;
                 while (i == ((dma.GetControllBlockAddress() - allocated.GetPhysicalAddress(dmaCb)) / (2 * sizeof(DMAControllBlock)))) {
                     std::this_thread::sleep_for(std::chrono::microseconds(BUFFER_TIME / 10));
                 }
